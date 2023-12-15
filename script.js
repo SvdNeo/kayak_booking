18509d3804f13c6e66aad100176f9ea4f3038282
SvdNeo <sanjay.d@neoito.com>
2023-12-15 15:50:55 +0530

    accessed the input type

---
+++ script.js
@@ -0,0 +1,11 @@
+function bookFlight() {
+  
+    const travellers = document.getElementById("travellers").value;
+}
+
+let travellersData = {
+    'Adults': 0,
+    'Students': 0,
+    'Youths': 0,
+    'Children': 0
+};
